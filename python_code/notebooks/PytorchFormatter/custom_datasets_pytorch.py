e25ee59afa2a21a62f62d6f137d47c901f3e0058
Daniele <daniele.contessi@unitn.it>
2022-11-28 23:26:31 +0100

    New notebooks for computation

---
+++ python_code/notebooks/PytorchFormatter/custom_datasets_pytorch.py
@@ -0,0 +1,26 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+class CustomDataset(Dataset):
+  def __init__(self, df_train, df_test):
+    self.features = torch.FloatTensor(df_train.drop(['sensor_code','in_datetime'],axis=1).values)
+    self.target = torch.FloatTensor(df_test.drop(['sensor_code','in_datetime']+[f"val_{idx}" for idx in range(n_previsions)],axis=1).values)
+    mean = self.features.mean()
+    std = self.features.std()
+    self.features = (self.features-mean)/std
+    self.target = (self.target-mean)/std
+    self.sensor_code = df_train['sensor_code'].values
+    self.in_datetime = df_train['in_datetime'].values
+    
+    self.labels = torch.FloatTensor(df_test[[f"val_{idx}" for idx in range(n_previsions)]].values)
+
+  def __len__(self):
+    return len(self.sensor_code)
+
+  def __getitem__(self, idx):
+    sensor_code = self.sensor_code[idx]
+    date = self.in_datetime[idx]
+    features = self.features[idx]
+    labels = self.labels[idx]
+    target = self.target[idx]
+    return {"sensor_code": sensor_code, "date":date,"features": features, "target":target,"labels":labels}
